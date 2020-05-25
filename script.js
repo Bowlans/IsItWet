3dcb7f0a4cf895162d261dbdfc50e71830ed7b1f
Bryn Thomas <themagicofgreenseeds@gmail.com>
2020-05-25 22:24:56 +0000

    added api support

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Weather</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+    
+    </script>
+</head>
+<body>
+
+</body>
+</html>
